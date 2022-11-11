35434be543a68678982d852400dc5f3fa236e4ff
oatmeeel <103972617+oatmeeel@users.noreply.github.com>
2022-11-10 17:04:36 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,3 +5,22 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+cd student-submission
+CPATH = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+
+if [[! -f ListExamples.java]]
+then
+    echo "ListExamples.java does not exist"
+    exit
+fi
+
+cd list-examples-grader
+cp TestListExamples.java student-submission
+cd student-submission
+
+set +e
+
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+exit
